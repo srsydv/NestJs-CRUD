dd4025ea50eae759b9a49ad6a5d0524c25a9ff67
srsydv <shrishofficial18@gmail.com>
2026-05-27 10:12:40 +0530

    Add user module with service and tests; integrate user creation into authentication flow

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Post } from '@nestjs/common';
-
+import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
+
+    constructor(private authService: AuthService) {}
     @Post('/register')
     register(){
-        return {message: "User registered successfully"};
+        const result = this.authService.registerUser();
+        return result;
+        // return {message: "User registered successfully"};
     }
 }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService: UserService) {}
+    
+    registerUser() {
+        const result = this.userService.createUser();
+        return result;
+    }
+}
