a09e0b6cff50e32c428d81dce7e4653d4e6bc8dc
srsydv <shrishofficial18@gmail.com>
2026-05-27 10:52:04 +0530

    Integrate bcrypt for password hashing in user registration; update AuthService and UserService to handle password securely and modify AuthController to accept registration data.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
-
+import bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
     constructor(private userService: UserService) {}
     
-    registerUser() {
-        const result = this.userService.createUser();
+    async registerUser(body: any) {
+        console.log("service body", body);
+        const hashedPassword = await bcrypt.hash(body.password, 10);
+        const result = this.userService.createUser({...body, password: hashedPassword});
         return result;
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-    createUser() {
+    createUser(body: any) {
         return {message: "User created successfully"};
     } 
 }
